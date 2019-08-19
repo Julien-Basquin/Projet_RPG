49f218748b35e2ceeeae6f0eacc77d29922fdfa0
Simon Strzelecki <simonstrzelecki@gmail.com>
2019-08-20 00:16:53 +0200

    set configuration

--- src/generateur/Launcher.java
+++ src/generateur/Launcher.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import util.DateUtile;
+import util.LanguageManager;
+import util.LoadConfiguration;
 
 /**
  * Main du générateur, sert à lancer l'application.
@@ -17,28 +19,29 @@ public class Launcher {
 	
 	public static LanguageManager languageManager;
 	
-	public static void main (String[] arg) {
-		
+	public static void main (String[] arg) {	
+		// Set configuration
+		LoadConfiguration configuration = new LoadConfiguration();
 		// Set date for logging system
 		DateUtile dateUtile = new DateUtile();
 		// Set log-level for logging system
-		System.setProperty("logLevel", "DEBUG");
+		System.setProperty("logLevel", configuration.getString("Config.General.LogLevel"));
 		// Set Log4j properties
-		String log4jConfPath = "./ressources/log4j.properties";
+		String log4jConfPath = configuration.getString("Config.General.Log4J.Path");
 		PropertyConfigurator.configure(log4jConfPath);
 		// Set language
-		languageManager = new LanguageManager("fr");
+		languageManager = new LanguageManager(configuration.getString("Config.General.Laguage"));
 		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		// max 60 fps
-		config.foregroundFPS = 60;
-		// set resolution to HD ready (1920 x 1080)
-		config.width = 1920;
-		config.height = 1080;
+		// max FPS
+		config.foregroundFPS = configuration.getInteger("Config.General.Max.FPS");
+		// set resolution
+		config.width = configuration.getInteger("Config.General.Resolution.Width");
+		config.height = configuration.getInteger("Config.General.Resolution.Height");
 		// set full screen
-		config.fullscreen = true;
+		config.fullscreen = configuration.getBoolean("Config.General.Fullscreen");
 		// set VSync
-		config.vSyncEnabled = true;
+		config.vSyncEnabled = configuration.getBoolean("Config.General.VSyncEnabled");
 		new LwjglApplication(new Generator(), config);
 	}
 }
