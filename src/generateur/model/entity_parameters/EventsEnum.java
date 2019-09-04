73d9e50aa6607b7aa76853d43d23c544bfd3dfe3
Julien_B <julien.basquin@hotmail.com>
2019-09-04 09:22:36 +0200

    Ajout de l'enum des évènements du générateur

---
+++ src/generateur/model/entity_parameters/EventsEnum.java
@@ -0,0 +1,17 @@
+package generateur.model.entity_parameters;
+
+/**
+ * Enum des évènements possibles pour le générateur
+ * 
+ * @author Julien B.
+ */
+
+public enum EventsEnum {
+	ADD,
+	DELETE,
+	MOVE,
+	EDIT,
+	INIT,
+	UNDO,
+	REDO;
+}
