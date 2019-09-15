89e107ea68f2bee7ff91459eb41c4b880b3e4269
Simon Strzelecki <simonstrzelecki@gmail.com>
2019-09-15 23:52:30 +0200

    add NodeModification Class

---
+++ src/generateur/view/dialog/entity_parameters/graph/NodeModificationDialogue.java
@@ -0,0 +1,15 @@
+package generateur.view.dialog.entity_parameters.graph;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+import generateur.Launcher;
+
+public class NodeModificationDialogue extends Dialog {
+
+	public NodeModificationDialogue(Skin skin) {
+		super(Launcher.languageManager.getProperty("Global.Confirm"), skin);
+		// TODO Auto-generated constructor stub
+	}
+
+}
